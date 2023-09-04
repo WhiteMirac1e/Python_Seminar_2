4c829b0db2a35b922249831f8c785032fc22acf4
WhiteMirac1e <115178943+WhiteMirac1e@users.noreply.github.com>
2023-09-04 16:22:53 +0300

    Create dz_task_1.py

---
+++ dz_task_1.py
@@ -0,0 +1,17 @@
+number = int(input("Введите число: "))
+BASE = 16
+original_number = number
+result_number = ''
+while number:
+    if number%BASE < 10:
+        result_number =str(number%BASE) + result_number
+    elif number%BASE == 10: result_number ='A' + result_number
+    elif number%BASE == 11: result_number ='B' + result_number
+    elif number%BASE == 12: result_number ='C' + result_number
+    elif number%BASE == 13: result_number ='D' + result_number
+    elif number%BASE == 14: result_number ='E' + result_number
+    elif number%BASE == 15: result_number ='F' + result_number
+    elif number%BASE == 16: result_number ='G' + result_number
+    number //= BASE
+print(f'Число {original_number} В {BASE}-ичной системе счисления будет {result_number}')
+print(hex(original_number))
